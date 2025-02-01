ab6550ca3b76cf30cd2bbdb82a1186d00eb799b9
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-01-31 21:19:04 -0700

    test: init Domain.Tests project

--- src/Domain/Domain/DomainRegistrationExtensions.cs
+++ src/Domain/Domain/DomainRegistrationExtensions.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Domain;
 
+[ExcludeFromCodeCoverage]
 public static class DomainRegistrationExtensions
 {
     public static IServiceCollection AddDomainServices(this IServiceCollection services)
