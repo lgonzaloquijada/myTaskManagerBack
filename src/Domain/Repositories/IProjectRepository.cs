87bff7b74dfde50cc4fbcf2c7c7060d023df080e
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-03-03 23:39:22 -0700

    refactor: changing IProjectRepository to implement IRepository.

--- src/Domain/Repositories/IProjectRepository.cs
+++ src/Domain/Repositories/IProjectRepository.cs
@@ -2,11 +2,6 @@ using Domain.Entities;
 
 namespace Domain.Repositories;
 
-public interface IProjectRepository
+public interface IProjectRepository : IRepository<Project>
 {
-    Task<List<Project>> GetAll();
-    Task<Project?> GetById(int id);
-    Task<Project> Create(Project project);
-    Task<Project> Update(Project project);
-    Task<Project> Delete(Project project);
 }
