b7e0f3e8ff0afdf3172286b78652653ebfd5298a
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-03-03 23:35:58 -0700

    feat: adding Project domain.

---
+++ src/Domain/Entities/Project.cs
@@ -0,0 +1,41 @@
+using Domain.Models;
+
+namespace Domain.Entities;
+public class Project : Entity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int Status { get; set; }
+    public int Priority { get; set; }
+    public int ManagerId { get; set; }
+    public User Manager { get; set; }
+    public ICollection<Task> Tasks { get; set; }
+
+    public Project()
+    {
+        Name = string.Empty;
+        Description = string.Empty;
+        StartDate = DateTime.Now;
+        EndDate = DateTime.Now;
+        Status = 0;
+        Priority = 0;
+        ManagerId = 0;
+        Manager = new User();
+        Tasks = new List<Task>();
+    }
+
+    public Project(string name, string description, DateTime startDate, DateTime endDate, int status, int priority, int managerId, User manager, ICollection<Task> tasks)
+    {
+        Name = name;
+        Description = description;
+        StartDate = startDate;
+        EndDate = endDate;
+        Status = status;
+        Priority = priority;
+        ManagerId = managerId;
+        Manager = manager;
+        Tasks = tasks;
+    }
+}
